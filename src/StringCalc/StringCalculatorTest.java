5d2f5117c278eeaa99d969ed712ff36559b89ee0
Sigmar Sigurðarson <sigmarb@clients-pool-1.nat.ru.is>
2015-10-12 21:07:28 +0000

    emptystring test

--- src/StringCalc/StringCalculatorTest.java
+++ src/StringCalc/StringCalculatorTest.java
@@ -1,5 +1,20 @@
 package StringCalc;
 
-public class StringCalculatorTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+public class StringCalculatorTest 
+{
+	public static void main(String args[])
+	{
+		org.junit.runner.JUnitCore.main("StringCalc.StringCalculator");
+	}
+	
+	@Test
+	public void testEmptyString()
+	{
+		assertEquals(0, StringCalculator.add(""));
+	}
+	
+	
 }
